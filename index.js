f94b3adc9ff85975aff998d1804c15b105ed8128
Robert Weekes <weekes.robert@gmail.com>
2024-06-05 22:06:07 -0700

    done with index file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// const sayMessage = require('./messages/say-message');
 const { sayHelloTo } = require('./send-messages/say-hello-to');
 const giveMessageToMrsPotato = require('./send-messages/give-message-to-mrs-potato');
-const { msg1, msg2, msg3 } = require('./messages/index');
+const { message1: msg1, message2: msg2, message3: msg3 } = require('./messages/index');
+// const {msg1} = require('./messages');
+// const {msg2} = require('./messages');
+// const {msg3} = require('./messages');
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
