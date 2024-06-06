a0091f766988039a76c9187fada8b694e521b700
Robert Weekes <weekes.robert@gmail.com>
2024-06-05 19:03:57 -0700

    almost done with index file imports

--- messages/index.js
+++ messages/index.js
@@ -1,6 +1,14 @@
+// const helloMessage = require('./hello-message');
+// const sayMessage = require('./say-message');
+// const { sayHelloTo } = require('../send-messages/say-hello-to');
+// const giveMessageToMrsPotato = require('../send-messages/say-hello-to');
+
 const message1 = "You are looking very spudly today!";
 const message2 = "There's a good deal on mattresses tomorrow.";
 const message3 = "Do you like to do things in unnecessarily complicated ways?";
+
+// giveMessageToMrsPotato("Hi Buzz");
+
 
 //!!START SILENT
 module.exports = {
